be793bf7f4342e3e3eb95547ca2dcb546981fceb
Dimitri 'skypers' Sabadie <dimitri.sabadie@gmail.com>
2014-07-31 21:29:53 +0200

    Renderer as data instead as typeclass and Interpreter.

---
+++ Photon/Core/Renderer.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE RankNTypes, TypeFamilies #-}
+
+module Photon.Core.Renderer where
+
+import Control.Monad.Trans ( MonadIO )
+import Photon.Core.Entity ( Entities )
+import Photon.Core.Scene ( Scene, IndexPath )
+
+data Renderer frame = Renderer {
+    -- |
+    render :: forall a. Scene a -> Entities IndexPath -> frame
+    -- |
+  , display :: (MonadIO m) => frame -> m ()
+    -- |
+  , screenshot :: (MonadIO m) => FilePath -> frame -> m ()
+  }
