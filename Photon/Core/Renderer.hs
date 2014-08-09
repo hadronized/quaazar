e10ae537a4ee23ef617b85979a658d17b76aa7f0
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2014-08-09 15:44:54 +0200

    Documentation.

--- Photon/Core/Renderer.hs
+++ Photon/Core/Renderer.hs
@@ -6,11 +6,15 @@ import Control.Monad.Trans ( MonadIO )
 import Photon.Core.Entity ( Entities )
 import Photon.Core.Scene ( IndexPath )
 
+-- |Renderer are plain data that host rendering functions. They’re generated
+-- for a specific `Scene a`.
 data Renderer frame = Renderer {
-    -- |
+    -- |Render scene entities into a frame. This frame is just a rendered
+    -- version of the scene, nothing more.
     render :: Entities IndexPath -> frame
-    -- |
+    -- |Displaye a rendered scene (frame). That function should render the
+    -- frame on the screen / dedicated area for rendering.
   , display :: (MonadIO m) => frame -> m ()
-    -- |
+    -- |Write a frame in a file as a PNG image.
   , screenshot :: (MonadIO m) => FilePath -> frame -> m ()
   }
