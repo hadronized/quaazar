0e84d8d58fac63063dded56f4152b95fbd2e0b16
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2014-08-11 11:39:47 +0200

    Typo.

--- Photon/Core/Renderer.hs
+++ Photon/Core/Renderer.hs
@@ -12,8 +12,8 @@ data Renderer frame = Renderer {
     -- |Render scene entities into a frame. This frame is just a rendered
     -- version of the scene, nothing more.
     render :: Entities IndexPath -> frame
-    -- |Displaye a rendered scene (frame). That function should render the
-    -- frame on the screen / dedicated area for rendering.
+    -- |Display a rendered scene (frame). That function should render the
+    -- frame onto the screen / dedicated area for rendering.
   , display :: frame -> IO ()
     -- |Write a frame in a file as a PNG image.
   , screenshot :: FilePath -> frame -> IO ()
