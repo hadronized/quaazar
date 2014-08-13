9ec83dc094e1369c2186bfba5afc2caffbfb0b49
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2014-08-13 15:11:04 +0200

    PostFX and postfx.

--- Photon/Core/Renderer.hs
+++ Photon/Core/Renderer.hs
@@ -1,6 +1,8 @@
 module Photon.Core.Renderer (
     -- * Renderer
     Renderer(..)
+    -- * Post-process
+  , PostFX(..)
   ) where
 
 import Photon.Core.Entity ( Entities )
@@ -11,10 +13,16 @@ import Photon.Core.Scene ( IndexPath )
 data Renderer frame = Renderer {
     -- |Render scene entities into a frame. This frame is just a rendered
     -- version of the scene, nothing more.
-    render :: Entities IndexPath -> frame
+    render     :: Entities IndexPath -> frame
+    -- |Apply a list of post-processes on a frame, and return the new frame.
+  , postfx     :: [PostFX frame] -> frame -> frame
     -- |Display a rendered scene (frame). That function should render the
     -- frame onto the screen / dedicated area for rendering.
-  , display :: frame -> IO ()
+  , display    :: frame -> IO ()
     -- |Write a frame in a file as a PNG image.
   , screenshot :: FilePath -> frame -> IO ()
   }
+
+-- |A post-process effect is an endormophism between two frames. It’s used to
+-- modify a frame.
+newtype PostFX frame = PostFX { runPostFX :: frame -> frame }
