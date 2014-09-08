20baf3f114d0f57fd277ec87995a3dea889a4f49
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2014-09-08 10:29:42 +0200

    Documentation.

--- Photon/Core/Color.hs
+++ Photon/Core/Color.hs
@@ -1,5 +1,26 @@
+-----------------------------------------------------------------------------
+-- |
+-- Copyright   : (C) 2014 Dimitri Sabadie
+-- License     : BSD3
+--
+-- Maintainer  : Dimitri Sabadie <dimitri.sabadie@gmail.com>
+-- Stability   : experimental
+-- Portability : portable
+--
+-- Make a better world with a colored-one! This module exports a very
+-- straight-forward type, 'Color', which is a 4-float vector.
+--
+-- > For now, any vector-related function/type uses "linear"’s vectors.
+----------------------------------------------------------------------------
+
 module Photon.Core.Color where
 
 import Linear.V4 ( V4 )
 
+-- |A color is a 4-float vector. The four channels are:
+--
+--   - *red*
+--   - *green*
+--   - *blue*
+--   - *alpha*
 type Color = V4 Float
