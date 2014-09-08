ca2d57e45a7955ceb6ff001141fdfdaebab878dd
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2014-09-08 10:16:23 +0200

    Documentation.

--- Photon/Utils/Directory.hs
+++ Photon/Utils/Directory.hs
@@ -1,3 +1,15 @@
+-----------------------------------------------------------------------------
+-- |
+-- Copyright   : (C) 2014 Dimitri Sabadie
+-- License     : BSD3
+--
+-- Maintainer  : Dimitri Sabadie <dimitri.sabadie@gmail.com>
+-- Stability   : experimental
+-- Portability : portable
+--
+-- This module adds a few stuff to "System.Directory".
+----------------------------------------------------------------------------
+
 module Photon.Utils.Directory (
     -- * Sorting files
     partitionFileDir
@@ -6,6 +18,8 @@ module Photon.Utils.Directory (
 import Photon.Control.Monad ( partitionM )
 import System.Directory ( doesDirectoryExist, doesFileExist )
 
+-- |Partition files into a list of *files* and a list of *directories*.
+-- If a file is not a directory, it’s considered as a file.
 partitionFileDir :: [FilePath] -> IO ([FilePath],[FilePath])
 partitionFileDir files = do
     (f,r) <- partitionM doesFileExist files
