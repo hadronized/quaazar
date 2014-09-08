12e5fb44911cb3e193cda5fe3e09e42cfe769d8f
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2014-09-08 14:44:02 +0200

    Documentation.

--- Photon/Core/Light.hs
+++ Photon/Core/Light.hs
@@ -1,3 +1,23 @@
+-----------------------------------------------------------------------------
+-- |
+-- Copyright   : (C) 2014 Dimitri Sabadie
+-- License     : BSD3
+--
+-- Maintainer  : Dimitri Sabadie <dimitri.sabadie@gmail.com>
+-- Stability   : experimental
+-- Portability : portable
+--
+-- Lighting is required in order to see non-emissive objects.
+--
+-- 'Light' exposes the light type. You can find these types of light:
+--
+--   - 'Omni': omni lights – a.k.a. point lights – are lights that emit in
+--     all directions
+--
+-- Whatever the type of a light, it holds lighting information via a value
+-- of type 'LightProperties'.
+----------------------------------------------------------------------------
+
 module Photon.Core.Light (
     -- * Light
     Light(..)
@@ -11,16 +31,22 @@ module Photon.Core.Light (
 import Control.Lens
 import Photon.Core.Color ( Color )
 
+-- |Light. Extra information (cuttoff angle for instance) can be added
+-- regarding the type of the light.
 data Light
-  = Omni LightProperties
+  = Omni LightProperties -- ^ Omni light
     deriving (Eq,Show)
 
+-- |Lighting properties. This type is shared by lights.
 data LightProperties = LightProperties {
-    -- |
+    -- |Color of the light.
     _ligColor     :: Color
-    -- |
+    -- |Shininess of the light. That property directly affects the
+    -- specular aspect of the light. The greater it is, the intense
+    -- the specular effect is.
   , _ligShininess :: Float
-    -- |
+    -- |Power of the light – a.k.a. radius. Used to alter the attenuation
+    -- of the light over distance.
   , _ligPower     :: Float
   } deriving (Eq,Show)
 
