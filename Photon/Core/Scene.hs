78fda96182c7838cf9e8f8b1a983e28d3871fe15
phaazon <dimitri.sabadie@gmail.com>
2014-09-08 18:20:35 +0200

    Backticks to quotes.

--- Photon/Core/Scene.hs
+++ Photon/Core/Scene.hs
@@ -55,32 +55,32 @@ data Scene a = Scene {
   } deriving (Eq,Show)
 
 -- |Names are unique identifiers used to identify objects in a scene. For
--- instance, `Scene String` has `String` names. `Scene Integer` has `Integer`
+-- instance, 'Scene String' has 'String' names. 'Scene Integer' has 'Integer'
 -- names, and so on. That representation is handy, but it will obviously not
 -- fit realtime expectations.
 --
--- So comes `IndexPath`, a special name. It’s especially great because it comes
+-- So comes 'IndexPath', a special name. It’s especially great because it comes
 -- with a set of built-in properties. First, it enables you to access scene
 -- objects in amortized constant time (*O(1)*). Secondly, any type of name used
--- in a scene can be converted to an `IndexPath`. This is an extremely great
+-- in a scene can be converted to an 'IndexPath'. This is an extremely great
 -- property, and means you can use any kind of name you want in your scene, you
--- still have the possibility to optimize the names away and use `IndexPath`es.
+-- still have the possibility to optimize the names away and use 'IndexPath'es.
 --
--- Note: you can’t build `IndexPath` on your own. Feel free to read the
--- documentation of the `indexPath` function for further details.
+-- Note: you can’t build 'IndexPath' on your own. Feel free to read the
+-- documentation of the 'indexPath' function for further details.
 newtype IndexPath = IndexPath { unIndexPath :: [Int] } deriving (Eq,Ord,Show)
 
 makeLenses ''Scene
 makeLenses ''IndexPath
 
--- |Map a name to its `IndexPath` representation. That function should be
+-- |Map a name to its 'IndexPath' representation. That function should be
 -- partially applied in order to get a mapping function
--- (`a -> Maybe IndexPath`):
+-- ('a -> Maybe IndexPath'):
 --
 --     indexPath scene
 --
 -- You can then use that to change all names of your scene at once or change
--- similar structure’s names (see `EntityGraph`).
+-- similar structure’s names (see 'EntityGraph').
 --
 --     fmap (fromJust . indexPath scene) scene
 indexPath :: (Ord a) => Scene a -> Map a IndexPath
