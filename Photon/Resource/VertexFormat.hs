b7b657df28e5008dbafc8b4602317fd2d6d88a91
phaazon <dimitri.sabadie@gmail.com>
2014-09-11 20:24:45 +0200

    Added VertexFormat as a resource.

---
+++ Photon/Resource/VertexFormat.hs
@@ -0,0 +1,29 @@
+-----------------------------------------------------------------------------
+-- |
+-- Copyright   : (C) 2014 Dimitri Sabadie
+-- License     : BSD3
+--
+-- Maintainer  : Dimitri Sabadie <dimitri.sabadie@gmail.com>
+-- Stability   : experimental
+-- Portability : portable
+--
+-- Vertex formats can be considered as resources. This module exports
+-- anything you might need to deal with such resources.
+----------------------------------------------------------------------------
+
+module Photon.Resource.VertexFormat (
+    -- *
+  ) where
+
+import Control.Monad.Trans ( MonadIO, liftIO )
+import Control.Monad.Except ( MonadError )
+import Photon.Core.Vertex ( VertexFormat, vertexFormatParser )
+import Photon.Utils.Log ( MonadLogger )
+import Photon.Utils.Parsing
+
+loadVertexFormat :: (MonadLogger m,MonadError String m,MonadIO m)
+                 => FilePath
+                 -> m VertexFormat
+loadVertexFormat path = do
+    vf <- liftIO $ parseFromFile vertexFormatParser path
+    either (throwError . show) return vf
