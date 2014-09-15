2021aed47d178357e98affbcda9c58c8c451d3ae
phaazon <dimitri.sabadie@gmail.com>
2014-09-15 09:30:39 +0200

    LightProperties now has its own module.

--- Photon/Core/Light.hs
+++ Photon/Core/Light.hs
@@ -21,33 +21,12 @@
 module Photon.Core.Light (
     -- * Light
     Light(..)
-    -- * Light properties
-  , LightProperties(LightProperties)
-  , ligColor
-  , ligShininess
-  , ligPower
   ) where
 
-import Control.Lens
-import Photon.Core.Color ( Color )
+import Photon.Core.LightProperties
 
 -- |Light. Extra information (cuttoff angle for instance) can be added
 -- regarding the type of the light.
 data Light
   = Omni LightProperties -- ^ Omni light
     deriving (Eq,Show)
-
--- |Lighting properties. This type is shared by lights.
-data LightProperties = LightProperties {
-    -- |Color of the light.
-    _ligColor     :: Color
-    -- |Shininess of the light. That property directly affects the
-    -- specular aspect of the light. The greater it is, the intense
-    -- the specular effect is.
-  , _ligShininess :: Float
-    -- |Power of the light – a.k.a. radius. Used to alter the attenuation
-    -- of the light over distance.
-  , _ligPower     :: Float
-  } deriving (Eq,Show)
-
-makeLenses ''LightProperties
