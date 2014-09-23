cdceec148cfdf729ee04223ee78f3239cafd4fa0
phaazon <dimitri.sabadie@gmail.com>
2014-09-23 23:52:14 +0200

    Turn Vertex and Vertices to fixed components.

--- Photon/Core/Vertex.hs
+++ Photon/Core/Vertex.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -----------------------------------------------------------------------------
 -- |
 -- Copyright   : (C) 2014 Dimitri Sabadie
@@ -16,39 +18,45 @@
 -- optimize/whatever evil you plan to do with ;)
 ----------------------------------------------------------------------------
 
-module Photon.Core.Vertex (
-  ) where
+module Photon.Core.Vertex where
 
 import Control.Applicative
 import Control.Lens
-import Control.Monad ( forM_, void )
-import Control.Monad.Trans ( lift )
-import Control.Monad.Trans.State ( evalStateT, get, put )
 import Data.Aeson
-import Data.Aeson.Types ( Parser )
-import Data.List ( foldl1' )
-import Data.Scientific ( toBoundedInteger )
-import Data.Semigroup ( Semigroup(..) )
-import Data.Word ( Word8, Word32 )
-import Foreign.Marshal.Array ( advancePtr, allocaArray, copyArray )
-import Foreign.Ptr ( Ptr, castPtr )
-import Foreign.Storable ( sizeOf )
-import Numeric.Natural ( Natural )
-import qualified Foreign.Marshal.Array as FFI ( withArray )
+import Photon.Core.Normal ( Normal )
+import Photon.Core.Position ( Position )
+import Photon.Core.UV ( UV )
 
 data Vertex = Vertex {
     -- |Position.
-    vertexPosition :: Position
+    _vertexPosition :: Position
     -- |Normal.
-  , vertexNormal   :: Normal
+  , _vertexNormal   :: Normal
     -- |UV channels.
-  , vertexUVChans  :: [UV]
+  , _vertexUVChans  :: [UV]
   } deriving (Eq,Ord,Show)
+
+instance FromJSON Vertex where
+  parseJSON = withObject "vertex" $ \o ->
+    Vertex
+      <$> o .: "position"
+      <*> o .: "normal"
+      <*> o .: "uvs"
+
+makeLenses ''Vertex
 
 data Vertices
   = Interleaved [Vertex]
   | Deinterleaved [Position] [Normal] [[UV]]
     deriving (Eq,Show)
+
+instance FromJSON Vertices where
+  parseJSON = withObject "vertices" $ \o -> do
+    int <- o .: "interleaved" .!= True
+    if int then
+      Interleaved <$> o .: "values"
+      else
+        Deinterleaved <$> o .: "positions" <*> o .: "normals" <*> o .: "uvs"
 
 -- |Group '[Vertex]' into interleaved 'Vertices'.
 interleaved :: [Vertex] -> Vertices
@@ -58,26 +66,6 @@ interleaved = Interleaved
 -- DList to maximize append speed.
 -- |Group '[Vertex]' into deinterleaved 'Vertices'.
 deinterleaved :: [Vertex] -> Vertices
-deinterleaved = foldr1 rezip
+deinterleaved verts = let (p,n,uvs) = foldr rezip ([],[],[]) verts in Deinterleaved p n uvs
   where
     rezip (Vertex p n uvs) (ap,an,auvs) = (p:ap,n:an,uvs:auvs)
-
--- |Expose 'Vertices' as a C pointer. This function is the only way to
--- poke such lists through *FFI*.
---
--- The function that treats the C pointer takes a @Ptr Word8@ that represents
--- the 'Vertices' and its size in bytes.
-withDeinterleaved :: Vertices -> (Int -> Ptr Word8 -> IO a) -> IO a
-withDeinterleaved v f = do
-    allocaArray totalBytes $ \buf -> do
-      copyVC buf
-      f totalBytes buf
-  where
-    totalBytes = sum (map vertexCompBytes v)
-    copyVC b = void . flip evalStateT b $ do
-        forM_ v $ \c -> do
-          let bytes = vertexCompBytes c
-          p <- get
-          put $ p `advancePtr` bytes
-          let copy x = FFI.withArray x $ \bufx -> copyArray p (castPtr bufx)  bytes
-          lift $ foldVertexComp copy copy copy c 
