394238540b74cd3d20ca266d97c6565863622dc8
phaazon <dimitri.sabadie@gmail.com>
2014-09-23 23:55:26 +0200

    No more vertex formats / unresolved meshes in Available.

--- Photon/Resource/Available.hs
+++ Photon/Resource/Available.hs
@@ -18,31 +18,16 @@ import Data.Map as M ( Map, mapMaybe )
 import Photon.Core.Light ( Light )
 import Photon.Core.Mesh ( Mesh )
 import Photon.Core.Model ( Model )
-import Photon.Core.Vertex ( VertexFormat )
-import Photon.Resource.Mesh ( UnresolvedMesh(..) )
 
 -- |Expose available resources. See 'Resource' for further details about
 -- how to get resources from 'Available'.
 data Available = Available {
-    -- |Available vertex formats.
-    _vertexFormats    :: Map String VertexFormat
     -- |Available meshes.
-  , _meshes           :: Map String Mesh
+    _meshes           :: Map String Mesh
     -- |Available models.
   , _models           :: Map String Model
     -- |Available lights.
   , _lights           :: Map String Light
-    -- |Available unresolved meshes.
-  , _unresolvedMeshes :: Map String UnresolvedMesh
   }
 
 makeLenses ''Available
-
--- TODO resolution integration should not be (.~), but some kind
--- of union.
--- |Resolve unresolved dependencies.
-resolve :: Available -> Available
-resolve a = a & meshes .~ resolvedMeshes
-  where
-    resolvedMeshes = mapMaybe (flip resolveMesh vfs) (a^.unresolvedMeshes)
-    vfs            = a^.vertexFormats
