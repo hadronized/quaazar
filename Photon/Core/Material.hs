5061f63af2b919de6e3c09d4ecc03c7a067ec48b
phaazon <dimitri.sabadie@gmail.com>
2014-10-13 17:35:19 +0200

    Added albedo property for materials.

--- Photon/Core/Material.hs
+++ Photon/Core/Material.hs
@@ -27,15 +27,19 @@ import Photon.Core.Color ( Color )
 
 -- |Material.
 data Material = Material {
+    -- |Color of the material.
     _matColor     :: Color
     -- |Shininess of the material. That property directly affects the
     -- specular aspect of the material. The greater it is, the intense
     -- the specular effect is.
   , _matShininess :: Float
+    -- |Albedo of the material. That property controls the amount of
+    -- reflected light the surface emits.
+  , _matAlbeda    :: Float
   } deriving (Eq,Show)
 
 instance FromJSON Material where
   parseJSON = withObject "material" $ \o ->
-    Material <$> o .: "color" <*> o .: "shininess"
+    Material <$> o .: "color" <*> o .: "shininess" <*> o .: "albedo"
 
 makeLenses ''Material
