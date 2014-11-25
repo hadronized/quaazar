58a21e630eaa4745683f92d747635c2d1c935006
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2014-11-25 11:17:31 +0100

    Manager, EffectfulManage and new spawn and lose functions.

--- Photon/Core/Effect.hs
+++ Photon/Core/Effect.hs
@@ -15,26 +15,53 @@ module Photon.Core.Effect where
 import Control.Lens
 import Prelude hiding ( drop )
 
+-- |Handle used to uniquely represent managed values.
 type H = Int
 
+-- |'Managed a' is a value glued to a /handle/ ('H').
 data Managed a = Managed {
     _handle  :: H
   , _managed :: a
   } deriving (Eq,Show)
 
-makeLenses ''Managed
+-- |A 'Manager m' is a monad with two additional methods: 'manage' and 'drop'.
+--
+-- 'manage' is used to manage any value. It turns 'a' into 'Managed a'. That is
+-- pretty useful for /managers/ that want to track objects.
+--
+-- 'drop' is used when we want to lose track of a managed value.
+class (Monad m) => Manager m where
+  manage :: a -> m (Managed a)
+  drop   :: Managed a -> m ()
 
-class (Monad m) => Manage m where
-  manage :: m H
-  drop   :: H -> m ()
+-- |A 'Manager m' monad is able to create 'Managed a' values. However, we could
+-- also want to react to such change. Reactions are implemented in a special
+-- typeclass (see 'Effect'). In order to adapt to reactions, 'EffectfulManage'
+-- represents a type that can be reacted to when 'spawned' and 'lost'.
+--
+-- 'spawned' takes a brand new managed object and returns the corresponding
+-- 's' effect.
+--
+-- 'lost' takes a recently dropped object and returns the corresponding 'l'
+-- effect.
+--
+-- **Important note**: there’s only one instance of 'EffectulManage' per type.
+-- That means you can’t have two pairs of effects for a same type.
+class EffectfulManage a s l | a -> s l where
+  spawned :: Managed a -> s
+  lost    :: Managed a -> l
 
+-- |'Effect e m' is a monad 'm 'with a specific effect 'e'.
 class (Monad m) => Effect e m where
   effect :: e -> m ()
 
-spawn :: (Manage m) => a -> m (Managed a)
+-- |Effectful manage.
+spawn :: (Manager m,EffectfulManage a s l,Effect s m) => a -> m (Managed a)
 spawn a = do
-  h <- manage
-  return (Managed h a)
+  ma <- manage a
+  effect (spawned ma)
+  return ma
 
-lose :: (Manage m) => Managed a -> m ()
-lose (Managed h _) = drop h
+-- |Effectful drop.
+lose :: (Manager m,EffectfulManage a s l,Effect l m) => Managed a -> m ()
+lose ma = drop ma >> effect (lost ma)
