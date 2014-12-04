0442feab2e528ca86386c79fbb8bad9ea0d1a914
phaazon <dimitri.sabadie@gmail.com>
2014-12-04 12:04:39 +0100

    RenderMesh was moved into Core.Mesh.

--- src/Photon/Render/Renderer.hs
+++ src/Photon/Render/Renderer.hs
@@ -12,29 +12,19 @@
 module Photon.Render.Renderer (
     -- * Reaction
     RenderEffect(..)
-  , renderMesh
   , display
   , screenshot
   ) where
 
 import Control.Applicative
 import Photon.Core.Effect
-import Photon.Core.Entity ( Entity )
 import Photon.Core.Light ( Light )
-import Photon.Core.Mesh ( Mesh )
 import Photon.Render.PostFX ( PostFX )
 
 data RenderEffect
-  = RenderMesh (Managed Mesh) (Managed (Entity Mesh))
-  | Display
+  = Display
   | Screenshot FilePath
     deriving (Eq,Show)
-
-renderMesh :: (Effect RenderEffect m)
-           => Managed Mesh
-           -> Managed (Entity (Mesh))
-           -> m ()
-renderMesh m e = react (RenderMesh m e)
 
 display :: (Effect RenderEffect m) => m ()
 display = react Display
