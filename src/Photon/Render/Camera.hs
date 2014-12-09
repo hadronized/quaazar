cd7d6f2fdc66641b4c44a10c7036f1ffbec26af7
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2014-12-09 13:35:18 +0100

    GPUCamera.

---
+++ src/Photon/Render/Camera.hs
@@ -0,0 +1,28 @@
+-----------------------------------------------------------------------------
+-- |
+-- Copyright   : (C) 2014 Dimitri Sabadie
+-- License     : BSD3
+--
+-- Maintainer  : Dimitri Sabadie <dimitri.sabadie@gmail.com>
+-- Stability   : experimental
+-- Portability : portable
+--
+----------------------------------------------------------------------------
+
+module Photon.Render.Camera (
+    -- *
+  ) where
+
+import Photon.Core.Entity ( Entity )
+import Photon.Core.Projection ( Projection )
+import Photon.Render.GL.Entity ( cameraTransform )
+import Photon.Render.Semantics ( cameraProjectionSem, cameraViewSem )
+import Photon.Render.Shader
+
+data GPUCamera = GPUCamera { runGPUCamera :: GPUProgram -> IO () }
+
+gpuCamera :: Projection -> Entity -> IO GPUCamera
+gpuCamera proj ent = return . GPUCamera $ \program -> do
+  let sem = programSemantic program
+  sem cameraProjectionSem @?= projectionMatrix proj
+  sem cameraViewSem @?= cameraTransform ent
