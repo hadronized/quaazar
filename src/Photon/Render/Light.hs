91045127fef4076f8e8b5639e1eccd3eb51095af
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2014-12-09 12:01:38 +0100

    Implemented gpuLight with (@?=).

--- src/Photon/Render/Light.hs
+++ src/Photon/Render/Light.hs
@@ -15,7 +15,17 @@ module Photon.Render.Light (
   , gpuLight
   ) where
 
-newtype GPULight = GPULight { unGPULight :: Light } deriving (Eq,Show)
+import Photon.Render.Semantics ( lightCastShadowsSem, lightColorSem
+                               , lightPowerSem, lightRadiusSem, lightTypeSem )
+import Photon.Render.Shader ( GPUShader, programSemantic )
 
-gpuLight :: Light -> GPULight
-gpuLight = return . GPULight
+newtype GPULight = GPULight { runLight :: GPUShader -> IO () } deriving (Eq,Show)
+
+gpuLight :: Light -> IO GPULight
+gpuLight (Light t col power radius castShadows) = return . GPULight $ \program -> do
+  let sem = programSemantic program
+  sem lightCastShadowsSem @?= castShadows
+  sem lightColorSem @?= col
+  sem lightPowerSem @?= power
+  sem lightRadiusSem @?= radius
+  sem lightTypeSem @?= t
