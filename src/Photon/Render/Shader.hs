b4f9701b908ff995bf63558a6304fe9b26b9b0e9
phaazon <dimitri.sabadie@gmail.com>
2014-12-12 22:00:11 +0100

    Changed GPUProgram to a more “closure-oriented” idea..

--- src/Photon/Render/Shader.hs
+++ src/Photon/Render/Shader.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RankNTypes #-}
+
 -----------------------------------------------------------------------------
 -- |
 -- Copyright   : (C) 2014 Dimitri Sabadie
@@ -11,33 +13,19 @@
 
 module Photon.Render.Shader where
 
-import Control.Lens ( makeLenses )
 import Control.Monad.Error.Class ( MonadError )
 import Control.Monad.Trans ( MonadIO(..) )
-import Data.Traversable ( traverse )
-import Data.Vector ( Vector, (!?), fromList )
-import Graphics.Rendering.OpenGL.Raw
-import Photon.Render.GL.Shader ( Program, ShaderType(..), Uniform, Uniformable
-                               , genProgram, genShader, getUniformLocation
-                               , uniform )
+import Photon.Render.GL.Shader ( ShaderType(..), Uniform, Uniformable
+                               , genProgram, genShader, getUniform )
 import qualified Photon.Render.GL.Shader as GL ( useProgram )
 import Photon.Utils.Log ( MonadLogger )
 
 data GPUProgram = GPUProgram {
-    _gpuShaderProgram   :: Program
-  , _gpuShaderSemantics :: Vector GLint
+    useProgram :: IO ()
+  , programSemantic :: (Uniformable a) => String -> IO (Uniform a)
   }
 
-makeLenses ''GPUProgram
-
-gpuProgram :: (MonadIO m,MonadLogger m,MonadError String m) => [(ShaderType,String)] -> [String] -> m GPUProgram
-gpuProgram shaders semantics = do
+gpuProgram :: (MonadIO m,MonadLogger m,MonadError String m) => [(ShaderType,String)] -> m GPUProgram
+gpuProgram shaders = do
   program <- mapM (uncurry genShader) shaders >>= genProgram
-  semantics' <- liftIO . fmap fromList $ traverse (getUniformLocation program) semantics
-  return (GPUProgram program semantics')
-
-programSemantic :: (Uniformable a) => GPUProgram -> Int -> Maybe (Uniform a)
-programSemantic (GPUProgram _ semantics) sem = fmap uniform (semantics !? sem)
-
-useProgram :: (MonadIO m) => GPUProgram -> m ()
-useProgram (GPUProgram p _) = liftIO (GL.useProgram p)
+  return $ GPUProgram (GL.useProgram program) (getUniform program)
