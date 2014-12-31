443d20422fb1a954c7617b9530f9fc9eefe52e85
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2014-12-31 12:01:29 +0100

    Formatted at 4 the name of committer.

--- src/Photon/Utils/Log.hs
+++ src/Photon/Utils/Log.hs
@@ -98,7 +98,7 @@ instance Show LogType where
 instance Show LogCommitter where
   show c = case c of
     CoreLog         -> "core"
-    BackendLog impl -> take 8 $ impl ++ repeat ' '
+    BackendLog impl -> take 4 $ impl ++ repeat ' '
     UserLog         -> "user"
 
 -- |Create a log.
