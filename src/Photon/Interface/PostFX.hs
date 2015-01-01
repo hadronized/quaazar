6f88d7dab806c294c25336e3f249d39cfa15327c
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-01-01 13:26:54 +0100

    Gamma correction post-process.

---
+++ src/Photon/Interface/PostFX.hs
@@ -0,0 +1,26 @@
+-----------------------------------------------------------------------------
+-- |
+-- Copyright   : (C) 2014 Dimitri Sabadie
+-- License     : BSD3
+--
+-- Maintainer  : Dimitri Sabadie <dimitri.sabadie@gmail.com>
+-- Stability   : experimental
+-- Portability : portable
+--
+----------------------------------------------------------------------------
+
+module Photon.Interface.PostFX (
+    -- * Gamma correction
+    gammaCorrection
+  ) where
+
+gammaCorrection :: PostFX
+gammaCorrection = PostFX $ unlines
+  [
+    "#version 330 core"
+  , "uniform sampler2D sourceTex;"
+  , "out vec4 frag;"
+  , "void main() {"
+  , "  frag = pow(texelFetch(sourceTex, ivec2(gl_FragCoord.xy), 0), vec4(1./2.2));"
+  , "}"
+  ]
