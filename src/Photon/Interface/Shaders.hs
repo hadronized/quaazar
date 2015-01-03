52254335f6f79a87b8827a07e4b37b0e82a5ed74
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-01-03 14:48:26 +0100

    Creating the geometry shader used to generate cube depthmap. Not complete yet tho.

--- src/Photon/Interface/Shaders.hs
+++ src/Photon/Interface/Shaders.hs
@@ -62,8 +62,8 @@ lightFS = unlines
   , "uniform vec3 ligCol;"
   , "uniform float ligPow;"
   , "uniform float ligRad;"
-  --, "layout (location = " ++ show lightProjViewSem ++ ") uniform mat4 ligProjView;"
-  --, "layout (location = 2) uniform samplerCube shadowmap;"
+  , "uniform mat4 lightProjViewSem;"
+  , "uniform samplerCube ligDepthmap;"
 
   , "out vec4 frag;"
 
@@ -102,11 +102,42 @@ lightCubeDepthmapVS = unlines
   , "layout (location = 0) in vec3 co;"
   , "layout (location = 1) in vec3 no;"
 
-  , "uniform mat4 projView;"
   , "uniform mat4 model;"
+  , "uniform vec3 ligPos";
 
   , "void main() {"
-  , "  gl_Position = projView * model * vec4(co,1.);"
+  , "  gl_Position = model * vec4(co,1.) - vec4(ligPos,1.);"
+  , "}"
+  ]
+
+-- The geometry shader is used because we’re doing a layered rendering in order
+-- to generate the whole cube depthmap in one pass. Each primitive (i.e.
+-- triangle) gets duplicate 6 times; one time per cubemap face.
+lightCubeDepthmapGS :: String
+lightCubeDepthmapGS = unlines
+  [
+    "#version 330 core"
+
+  , "layout (triangles) in;"
+  , "layout (triangle_strip, max_vertices = 18) out;"
+
+    -- Projection matrices (one per cubemap face).
+  , "const mat4[6] faceProjection = mat4[]("
+  , "    mat4(vec4(TODO),vec4(TODO),vec4(TODO),vec4(TODO))"
+  , "  , mat4(vec4(TODO),vec4(TODO),vec4(TODO),vec4(TODO))"
+  , "  , mat4(vec4(TODO),vec4(TODO),vec4(TODO),vec4(TODO))"
+  , "  , mat4(vec4(TODO),vec4(TODO),vec4(TODO),vec4(TODO))"
+  , "  );"
+
+  , "void main() {"
+  , "  for (int i = 0; i < 6; ++i) {"
+  , "    for (int j = 0; j < 3; ++j) {"
+  , "      gl_LayerID = i;"
+  , "      gl_Position = faceProjection[i] * gl_in.gl_Position;"
+  , "      EmitVertex();"
+  , "    }"
+  , "    EndPrimitive();"
+  , "  }"
   , "}"
   ]
 
