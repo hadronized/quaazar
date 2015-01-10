f67aa272f87bc3e8fffc4ce79a61779a729947f8
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-01-10 13:21:21 +0100

    This is holy huge (that’s what she said).

--- src/Photon/Render/Shader.hs
+++ src/Photon/Render/Shader.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE RankNTypes #-}
-
 -----------------------------------------------------------------------------
 -- |
 -- Copyright   : (C) 2014 Dimitri Sabadie
@@ -13,19 +11,6 @@
 
 module Photon.Render.Shader where
 
-import Control.Monad.Error.Class ( MonadError )
-import Control.Monad.Trans ( MonadIO(..) )
-import Photon.Render.GL.Shader ( ShaderType(..), Uniform, Uniformable
-                               , genProgram, genShader, getUniform )
-import qualified Photon.Render.GL.Shader as GL ( useProgram )
-import Photon.Utils.Log ( Log, MonadLogger )
+import Photon.Render.GL.Shader ( Program )
 
-data GPUProgram = GPUProgram {
-    useProgram :: IO ()
-  , programSemantic :: (Uniformable a) => String -> IO (Uniform a)
-  }
-
-gpuProgram :: (MonadIO m,MonadLogger m,MonadError Log m) => [(ShaderType,String)] -> m GPUProgram
-gpuProgram shaders = do
-  program <- mapM (uncurry genShader) shaders >>= genProgram
-  return $ GPUProgram (GL.useProgram program) (getUniform program)
+type GPUProgram = Program
