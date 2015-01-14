ec42d2752a962d6c3107d306baaae3e8c5ae822c
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-01-15 00:20:14 +0100

    Added shadows back.

--- src/Photon/Render/Forward/Lit.hs
+++ src/Photon/Render/Forward/Lit.hs
@@ -14,14 +14,15 @@ module Photon.Render.Forward.Lit where
 import Control.Lens
 import Data.Monoid
 import Graphics.Rendering.OpenGL.Raw
-import Photon.Core.Entity ( Entity )
+import Linear
+import Photon.Core.Entity
 import Photon.Render.Forward.Accumulation
 import Photon.Render.Forward.Lighting
 import Photon.Render.Forward.Shaded ( Shaded(..) )
 import Photon.Render.Forward.Shadowing
 import Photon.Render.GL.Framebuffer ( Target(..), bindFramebuffer )
 import Photon.Render.GL.Offscreen
-import Photon.Render.GL.Shader ( unused, useProgram )
+import Photon.Render.GL.Shader ( (@=), unused, useProgram )
 import Photon.Render.GL.Texture ( bindTextureAt )
 import Photon.Render.GL.VertexArray ( bindVertexArray )
 import Photon.Render.Light ( GPULight(..) )
@@ -37,8 +38,7 @@ lighten gpulig ent shd = Lit lighten_
   where
     lighten_ lighting shadowing accumulation = do
       purgeShadowingFramebuffer shadowing
-      --generateLightDepthmap shadowing -- TODO: reinsert shadow mapping
-      --  (concatMap snd meshes) lig lent
+      generateLightDepthmap lighting shadowing shd gpulig ent
       purgeLightingFramebuffer lighting
       withLight lighting shadowing shd gpulig ent
       accumulate lighting accumulation
@@ -54,6 +54,47 @@ withLight lighting shadowing shd gpulig ent = do
     unShaded shd lighting
   where
     lunis = lighting^.lightUniforms
+
+generateLightDepthmap :: Lighting
+                      -> Shadowing
+                      -> Shaded
+                      -> GPULight
+                      -> Entity
+                      -> IO ()
+generateLightDepthmap lighting shadowing shd lig lent = do
+    genDepthmap lig $ do
+      useProgram (shadowing^.shadowCubeDepthmapProgram)
+      ligProjViewsU @= lightProjViews
+      ligPosU @= ligPos
+      ligIRadU @= 1 / lightRadius lig
+      glDisable gl_BLEND
+      glEnable gl_DEPTH_TEST
+      unShadedNoMaterial shd lighting
+  where
+    proj = shadowing^.shadowProjection
+    sunis = shadowing^.shadowUniforms
+    ligProjViewsU = sunis^.shadowLigProjViewsU
+    ligPosU = sunis^.shadowLigPosU
+    ligIRadU = sunis^.shadowLigIRadU
+    modelU = sunis^.shadowModelU
+    lightProjViews = map ((proj !*!) . completeM33RotMat . fromQuaternion)
+      [
+        axisAngle yAxis (-pi/2) * axisAngle zAxis pi -- positive x
+      , axisAngle yAxis (pi/2) * axisAngle zAxis pi -- negative x
+      , axisAngle xAxis (-pi/2) -- positive y
+      , axisAngle xAxis (pi/2) -- negative y
+      , axisAngle yAxis pi * axisAngle zAxis pi -- positive z
+      , axisAngle zAxis (pi) -- negative z
+      ]
+    ligPos = lent^.entityPosition
+
+completeM33RotMat :: M33 Float -> M44 Float
+completeM33RotMat (V3 (V3 a b c) (V3 d e f) (V3 g h i)) =
+  V4
+    (V4 a b c 0)
+    (V4 d e f 0)
+    (V4 g h i 0)
+    (V4 0 0 0 1)
 
 accumulate :: Lighting -> Accumulation -> IO ()
 accumulate lighting accumulation = do
--- src/Photon/Render/Forward/Shaded.hs
+++ src/Photon/Render/Forward/Shaded.hs
@@ -17,14 +17,17 @@ import Photon.Render.Forward.Lighting
 import Photon.Render.Forward.Rendered ( Rendered(..) )
 import Photon.Render.Material ( GPUMaterial(..) )
 
-newtype Shaded = Shaded { unShaded :: Lighting -> IO () }
+data Shaded = Shaded {
+    unShaded :: Lighting -> IO ()
+  , unShadedNoMaterial :: Lighting -> IO ()
+  }
 
 instance Monoid Shaded where
-  mempty = Shaded . const $ return ()
-  Shaded f `mappend` Shaded g = Shaded $ \l -> f l >> g l
+  mempty = Shaded (const $ return ()) (const $ return ())
+  Shaded f g `mappend` Shaded f' g' = Shaded (\l -> f l >> g l) (\l -> f' l >> g' l)
 
 shade :: GPUMaterial -> Rendered -> Shaded
-shade gmat rdrd = Shaded shade_
+shade gmat rdrd = Shaded shade_ (unRendered rdrd)
   where
     shade_ lighting = do
         runMaterial gmat (lunis^.lightMatDiffAlbU) (lunis^.lightMatSpecAlbU)
