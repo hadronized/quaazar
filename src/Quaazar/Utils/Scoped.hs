5ed86943a79c37da3a463e9cc4c3c0ead80ea7b8
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-02-13 17:11:58 +0100

    MonadScoped and IOScopedT.

--- src/Quaazar/Utils/Scoped.hs
+++ src/Quaazar/Utils/Scoped.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE StandaloneDeriving #-}
+
 -----------------------------------------------------------------------------
 -- |
 -- Copyright   : (C) 2014 Dimitri Sabadie
@@ -9,8 +11,15 @@
 --
 ----------------------------------------------------------------------------
 
-module Quaazar.Utils.Scoped where
+module Quaazar.Utils.Scoped (
+    -- * Scoped monad
+    MonadScoped(..)
+    -- * IO scoped monad transformer
+  , IOScopedT
+  , runIOScopedT
+  ) where
 
+import Control.Applicative ( Applicative )
 import Control.Monad.Base ( MonadBase(..) )
 import Control.Monad.Trans ( MonadIO(..) )
 import Control.Monad.Trans.State ( StateT, modify, runStateT )
@@ -18,13 +27,15 @@ import Control.Monad.Trans.State ( StateT, modify, runStateT )
 class (MonadBase b m) => MonadScoped b m where
   scoped :: b () -> m ()
 
-type IOResourceT = StateT (IO ())
+newtype IOScopedT m a = IOScopedT { unIOScopedT :: StateT (IO ()) m a } deriving (Applicative,Functor,Monad)
 
-instance (MonadBase IO m) => MonadScoped IO (IOResourceT m) where
-  scoped a = modify (>>a)
+deriving instance (MonadBase IO m) => MonadBase IO (IOScopedT m)
 
-runIOResourceT :: (MonadIO m) => IOResourceT m a -> m a
-runIOResourceT scope = do
-  (a,cleanup) <- runStateT scope (return ())
+instance (MonadBase IO m) => MonadScoped IO (IOScopedT m) where
+  scoped a = IOScopedT $ modify (>>a)
+
+runIOScopedT :: (MonadIO m) => IOScopedT m a -> m a
+runIOScopedT scope = do
+  (a,cleanup) <- runStateT (unIOScopedT scope) (return ())
   liftIO cleanup
   return a
