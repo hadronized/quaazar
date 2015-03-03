832d2d231aeec75efb497b7591f167e84639e713
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-03-03 23:02:57 +0100

    Rendered now has information on Material.

--- src/Quaazar/Render/Forward/Rendered.hs
+++ src/Quaazar/Render/Forward/Rendered.hs
@@ -14,16 +14,25 @@ module Quaazar.Render.Forward.Rendered where
 import Data.Monoid ( Monoid(..) )
 import Linear ( M44 )
 import Quaazar.Core.Entity ( Entity )
+import Quaazar.Core.Material ( Albedo )
 import Quaazar.Render.GL.Shader ( Uniform )
+import Quaazar.Render.Material ( GPUMaterial(..) )
 import Quaazar.Render.Mesh ( GPUMesh(..) )
 
-newtype Rendered = Rendered { unRendered :: Uniform (M44 Float) -> IO () }
+newtype Rendered = Rendered {
+    unRendered :: Uniform (M44 Float) -- ^ model matrix
+               -> Uniform Albedo      -- ^ diffuse albedo
+               -> Uniform Albedo      -- ^ specular albedo
+               -> Uniform Float       -- ^ shininess
+               -> IO ()
+  }
 
 instance Monoid Rendered where
-  mempty = Rendered . const $ return ()
-  Rendered f `mappend` Rendered g = Rendered $ \l -> f l >> g l
+  mempty = Rendered $ \_ _ _ _ -> return ()
+  Rendered f `mappend` Rendered g =
+    Rendered $ \m df sp sh -> f m df sp sh >> g m df sp sh
 
-render :: GPUMesh -> Entity -> Rendered
-render gmsh ent = Rendered $ render_
-  where
-    render_ modelU = renderMesh gmsh modelU ent
+render :: GPUMesh -> GPUMaterial -> Entity -> Rendered
+render gmsh gmat ent = Rendered $ \modelU matDiffAlbU matSpecAlbU matShnU -> do
+    runMaterial gmat matDiffAlbU matSpecAlbU matShnU
+    renderMesh gmsh modelU ent
