fb5b6c3a5ad4e5f782038bac98266828d410778c
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-03-04 00:40:45 +0100

    Replaced Lit by Shaded in look.

--- src/Quaazar/Render/Forward/Looked.hs
+++ src/Quaazar/Render/Forward/Looked.hs
@@ -14,16 +14,16 @@ module Quaazar.Render.Forward.Looked where
 import Quaazar.Render.Camera ( GPUCamera )
 import Quaazar.Render.Forward.Accumulation
 import Quaazar.Render.Forward.Lighting
-import Quaazar.Render.Forward.Lit ( Lit(..) )
+import Quaazar.Render.Forward.Shaded ( Shaded(..) )
 import Quaazar.Render.Forward.Shadowing
 import Quaazar.Render.Forward.Viewport ( Viewport )
 
 newtype Looked = Looked { unLooked :: Lighting -> Shadowing -> Accumulation -> IO () }
 
-look :: GPUCamera -> Lit -> Looked
-look gpucam lit = Looked look_
+look :: GPUCamera -> Shaded -> Looked
+look gpucam shaded = Looked look_
   where
     look_ lighting shadowing accumulation = do
       purgeAccumulationFramebuffer accumulation
       pushCameraToLighting lighting gpucam
-      unLit lit lighting shadowing accumulation gpucam
+      unShaded shaded lighting shadowing accumulation gpucam
