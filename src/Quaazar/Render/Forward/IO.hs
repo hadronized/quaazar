f3b5b2760a977045526c630badd5f8cdfe6bf514
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2015-03-17 14:45:22 +0100

    Update IO.hs

--- src/Quaazar/Render/Forward/IO.hs
+++ src/Quaazar/Render/Forward/IO.hs
@@ -26,8 +26,8 @@ import Quaazar.Render.GL.Shader ( useProgram )
 import Quaazar.Render.GL.Texture ( bindTextureAt )
 import Quaazar.Render.GL.VertexArray ( bindVertexArray )
 
-display :: (MonadIO m) => ForwardRenderer -> GPUFrame -> Viewport -> Post -> m GPUTexture
-display (ForwardRenderer lighting shadowing accumulation window) gpuframe screenViewport pst = liftIO $ do
+displayInto :: (MonadIO m) => ForwardRenderer -> GPUFrame -> Viewport -> Post -> m GPUTexture
+displayInto (ForwardRenderer lighting shadowing accumulation window) gpuframe screenViewport pst = liftIO $ do
   setViewport screenViewport
   (finalOff,_) <- unPost pst lighting shadowing accumulation
   useProgram (accumulation^.accumProgram)
