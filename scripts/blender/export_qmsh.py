c5db2f183a93c22046867f5cd089ee9ef92d56e7
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-03-19 01:42:48 +0100

    Fixed the blender exporter.

--- scripts/blender/export_qmsh.py
+++ scripts/blender/export_qmsh.py
@@ -34,6 +34,12 @@ class QuaazarMeshExporter(bpy.types.Operator, ExportHelper):
     , default     = False
     , )
 
+  smoothNormals = BoolProperty (
+      name        = "Smooth normals"
+    , description = "Smooth normals?"
+    , default     = True
+    , )
+
   def execute(self, context):
     print("-- ----------------------- --")
     print("-- Quaazar Mesh JSON Export --")
@@ -46,7 +52,7 @@ class QuaazarMeshExporter(bpy.types.Operator, ExportHelper):
         print("W: '" + msh.name + "' is not elegible to export, please convert quadrangles to triangles")
       else:
         print("I: exporting '" + msh.name + "'")
-        phmsh = toQuaazarMesh(msh)
+        phmsh = toQuaazarMesh(msh, self.smoothNormals)
         fp = open(self.filepath, "w")
         fp.write(phmsh.toJSON(self.sparse))
         fp.close()
@@ -72,33 +78,56 @@ class QuaazarMesh:
     d = { "vertices" : {"interleaved" : True, "values" : self.vertices}, "vgroup" : { "grouping" : "triangles", "triangles" : self.vgroup } }
     return json.dumps(d, sort_keys=True, indent=i)
 
+# Check whether a mesh has only triangles.
 def hasOnlyTris(msh):
   for poly in msh.polygons:
     if len(poly.vertices) > 3:
       return False
   return True
 
-def toQuaazarMesh(msh):
-  vs = []
-  vg = []
-
-  # vertices
-  for vert in msh.vertices:
-    pos = [round_(vert.co[0]),round_(vert.co[1]),round_(vert.co[2])]
+# Build a vertex.
+def createVertex(vertices, vertID, smoothNormals):
+  vert = vertices[vertID]
+  co = [round_(vert.co[0]),round_(vert.co[1]),round_(vert.co[2])]
+  no = []
+  if smoothNormals:
     nor = [round_(vert.normal[0]),round_(vert.normal[1]),round_(vert.normal[2])]
-    vs.append([pos,nor,[]])
+  return [co,no,[]]
 
-  # vertex group
+# Look for a vertex. If it exists, return its ID. Otherwise, return None.
+def lookupVertex(vertices, vert):
+  try:
+    return vertices.index(vert)
+  except ValueError:
+    return None
+    
+# Record a new vertex for vertices / indices.
+def recordVertex(vertices, indices, vert):
+  vid = lookupVertex(vertices, vert)
+
+  if vid == None:
+    vid = len(vertices)
+    vertices.append(vert)
+
+  return vid
+  
+def toQuaazarMesh(msh, smoothNormals):
   i = 0
-  ll = len(msh.loops)
-  while i < ll:
-    vg.append([ msh.loops[i].vertex_index
-              , msh.loops[i+1].vertex_index
-              , msh.loops[i+2].vertex_index
-              ])
+  vnb = len(msh.loops)
+  vertices = []
+  indices = []
+  while i < vnb:
+    a = createVertex(msh.vertices, msh.loops[i].vertex_index, smoothNormals)
+    b = createVertex(msh.vertices, msh.loops[i+1].vertex_index, smoothNormals)
+    c = createVertex(msh.vertices, msh.loops[i+2].vertex_index, smoothNormals)
+    aID = recordVertex(vertices, indices, a)
+    bID = recordVertex(vertices, indices, b)
+    cID = recordVertex(vertices, indices, c)
+    indices.append([aID,bID,cID])
+
     i += 3
 
-  return QuaazarMesh(vs,vg)
+  return QuaazarMesh(vertices,indices)
 
 def round_(x):
   return round(x,6)
