7f2772f3de0dee4c2646872be424e765bea939da
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-03-21 23:21:18 +0100

    Support for integer 8bit greyscale alpha images.

--- src/Quaazar/Core/Texture.hs
+++ src/Quaazar/Core/Texture.hs
@@ -51,27 +51,44 @@ data TexelFormat
 
 imageToTexture :: (MonadError Log m) => DynamicImage -> m Texture
 imageToTexture dynim = case dynim of
-    ImageY8 img -> return $ fromGreyscale imax8 img
-    ImageY16 img -> return $ fromGreyscale imax16 img
-    {-
-    ImageYF img -> fromPixelF img
-    ImageYA8 img -> fromPixelYA8 img
-    ImageYA16 img -> fromPixelYA16 img
-    ImageRGB8 img -> fromPixelRGB8 img
-    ImageRGB16 img -> fromPixelRGB16 img
-    ImageRGBA8 img -> fromPixelRGBA8 img
-    ImgaeRGBA16 img -> fromPixelRGBA16 img
-    -}
-    _ -> throwError_ "unimplemented image format"
-  where
-    imax8 = 1 / 255
-    imax16 = 1 / realToFrac (maxBound :: Pixel16)
+  ImageY8 img -> return $ fromGreyscale imax8 img
+  ImageY16 img -> return $ fromGreyscale imax16 img
+  ImageYF img -> return $ fromPixelF img
+  ImageYA8 img -> return $ fromGreyscaleAlpha imax8 img
+  {-
+  ImageYA16 img -> fromPixelYA16 img
+  ImageRGB8 img -> fromPixelRGB8 img
+  ImageRGB16 img -> fromPixelRGB16 img
+  ImageRGBA8 img -> fromPixelRGBA8 img
+  ImgaeRGBA16 img -> fromPixelRGBA16 img
+  -}
+  _ -> throwError_ "unimplemented image format"
+ 
+imax8, imax16 :: Float
+imax8 = 1 / 255
+imax16 = 1 / realToFrac (maxBound :: Pixel16)
 
-fromGreyscale :: (Pixel a,Real (PixelBaseComponent a)) => Float -> Image a -> Texture
+fromGreyscale :: (Pixel a,Real (PixelBaseComponent a))
+              => Float
+              -> Image a
+              -> Texture
 fromGreyscale imax (Image w h pixels) =
     Texture (fromIntegral w) (fromIntegral h) R pixels'
   where
-    pixels' = fromList . fmap ((*imax) . realToFrac) $ toList pixels
+    pixels' = fromList . map ((*imax) . realToFrac) $ toList pixels
+
+fromPixelF :: Image PixelF -> Texture
+fromPixelF (Image w h pixels) =
+  Texture (fromIntegral w) (fromIntegral h) R . fromList $ toList pixels
+
+fromGreyscaleAlpha :: (Pixel a,Real (PixelBaseComponent a)) 
+                   => Float 
+                   -> Image a 
+                   -> Texture
+fromGreyscaleAlpha imax (Image w h pixels) =
+    Texture (fromIntegral w) (fromIntegral h) RG pixels'
+  where
+    pixels' = fromList . map ((*imax) . realToFrac) $ toList pixels
 
 throwError_ :: (MonadError Log m) => String -> m a
 throwError_ = throwError . Log ErrorLog CoreLog
