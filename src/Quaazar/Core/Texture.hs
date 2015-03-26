0a8c49e77819808a644e4ffd5ad2c2a1d1b9ffc9
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-03-26 17:01:14 +0100

    Support for YCbCr8 and made image converting more elegant.

--- src/Quaazar/Core/Texture.hs
+++ src/Quaazar/Core/Texture.hs
@@ -27,7 +27,6 @@ import Control.Lens
 import Data.Bifunctor ( first )
 import Data.Either.Combinators ( eitherToError )
 import Data.Vector ( Vector, fromList )
-import Data.Vector.Storable ( toList )
 import Numeric.Natural ( Natural )
 import Quaazar.Core.Loader ( Load(..) )
 import Quaazar.Utils.Log
@@ -66,73 +65,76 @@ data TexelFormat
 
 imageToTexture :: (MonadError Log m) => DynamicImage -> m Texture
 imageToTexture dynim = case dynim of
-  ImageY8 img -> return $ fromGreyscale imax8 img
-  ImageY16 img -> return $ fromGreyscale imax16 img
-  ImageYF img -> return $ fromPixelF img
-  ImageYA8 img -> return $ fromGreyscaleAlpha imax8 img
-  ImageYA16 img -> return $ fromGreyscaleAlpha imax16 img
-  ImageRGB8 img -> return $ fromRGB imax8 img
-  ImageRGB16 img -> return $ fromRGB imax16 img
-  ImageRGBF img -> return $ fromRGBF img
-  ImageRGBA8 img -> return $ fromRGBA imax8 img
-  ImageRGBA16 img -> return $ fromRGBA imax16 img
-  ImageYCbCr8 img -> return $ fromYCbCr8 img
+  ImageY8 img -> return $ convertImage y8Converter img
+  ImageY16 img -> return $ convertImage y16Converter img
+  ImageYF img -> return $ convertImage yfConverter img
+  ImageYA8 img -> return $ convertImage ya8Converter img
+  ImageYA16 img -> return $ convertImage ya16Converter img
+  ImageRGB8 img -> return $ convertImage rgb8Converter img
+  ImageRGB16 img -> return $ convertImage rgb16Converter img
+  ImageRGBF img -> return $ convertImage rgbfConverter img
+  ImageRGBA8 img -> return $ convertImage rgba8Converter img
+  ImageRGBA16 img -> return $ convertImage rgba16Converter img
+  ImageYCbCr8 img -> return $ convertImage ycc8Converter img
   _ -> throwError_ "unimplemented image format"
  
 imax8, imax16 :: Float
 imax8 = 1 / 255
 imax16 = 1 / realToFrac (maxBound :: Pixel16)
 
-fromGreyscale :: (Pixel a,Real (PixelBaseComponent a))
-              => Float
-              -> Image a
-              -> Texture
-fromGreyscale imax (Image w h pixels) =
-    Texture (fromIntegral w) (fromIntegral h) R pixels'
+convertImage :: forall a. (Pixel a) => (a -> [Float]) -> Image a -> Texture
+convertImage converter image@(Image w h _) = Texture w' h' texft pixels'
   where
-    pixels' = fromList . map ((*imax) . realToFrac) $ toList pixels
+    w' = fromIntegral w
+    h' = fromIntegral h
+    texft = case componentCount (undefined :: a) of
+      1 -> R
+      2 -> RG
+      3 -> RGB
+      _ -> error "convertImage: more-than-3-components pixel"
+    pixels' = fromList . concat $ [converter $ pixelAt image x y | x <- [0..w-1], y <- [0..h-1]]
 
-fromPixelF :: Image PixelF -> Texture
-fromPixelF (Image w h pixels) =
-  Texture (fromIntegral w) (fromIntegral h) R . fromList $ toList pixels
+y8Converter :: Pixel8 -> [Float]
+y8Converter y = [imax8 * realToFrac y]
 
-fromGreyscaleAlpha :: (Pixel a,Real (PixelBaseComponent a)) 
-                   => Float 
-                   -> Image a 
-                   -> Texture
-fromGreyscaleAlpha imax (Image w h pixels) =
-    Texture (fromIntegral w) (fromIntegral h) RG pixels'
+y16Converter :: Pixel16 -> [Float]
+y16Converter y = [imax16 * realToFrac y]
+
+yfConverter :: PixelF -> [Float]
+yfConverter y = [realToFrac y]
+
+ya8Converter :: PixelYA8 -> [Float]
+ya8Converter (PixelYA8 y a) = map ((*imax8) . realToFrac) [y,a]
+
+ya16Converter :: PixelYA16 -> [Float]
+ya16Converter (PixelYA16 y a) = map ((*imax16) . realToFrac) [y,a]
+
+rgb8Converter :: PixelRGB8 -> [Float]
+rgb8Converter (PixelRGB8 r g b) = map ((*imax8) . realToFrac ) [r,g,b]
+
+rgb16Converter :: PixelRGB16 -> [Float]
+rgb16Converter (PixelRGB16 r g b) = map ((*imax16) . realToFrac ) [r,g,b]
+
+rgbfConverter :: PixelRGBF -> [Float]
+rgbfConverter (PixelRGBF r g b) = map realToFrac [r,g,b]
+
+rgba8Converter :: PixelRGBA8 -> [Float]
+rgba8Converter (PixelRGBA8 r g b a) = map ((*imax8) . realToFrac) [r,g,b,a]
+
+rgba16Converter :: PixelRGBA16 -> [Float]
+rgba16Converter (PixelRGBA16 r g b a) = map ((*imax16) . realToFrac) [r,g,b,a]
+
+ycc8Converter :: PixelYCbCr8 -> [Float]
+ycc8Converter (PixelYCbCr8 y cb cr) =
+    [
+      y' + 1.402 * cr'
+    , y' - 0.34414 * cb' - 0.71414 * cr'
+    , y' + 1.772 * cb'
+    ]
   where
-    pixels' = fromList . map ((*imax) . realToFrac) $ toList pixels
-
-fromRGB :: (Pixel a,Real (PixelBaseComponent a))
-        => Float
-        -> Image a
-        -> Texture
-fromRGB imax (Image w h pixels) =
-    Texture (fromIntegral w) (fromIntegral h) RGB pixels'
-  where
-    pixels' = fromList . map ((*imax) . realToFrac) $ toList pixels
-
-fromRGBF :: Image PixelRGBF -> Texture
-fromRGBF (Image w h pixels) =
-  Texture (fromIntegral w) (fromIntegral h) RGB . fromList $ toList pixels
-
-fromRGBA :: (Pixel a,Real (PixelBaseComponent a))
-         => Float
-         -> Image a
-         -> Texture
-fromRGBA imax (Image w h pixels) =
-    Texture (fromIntegral w) (fromIntegral h) RGBA pixels'
-  where
-    pixels' = fromList . map ((*imax) . realToFrac) $ toList pixels
-
-fromYCbCr8 :: Image PixelYCbCr8 -> Texture
-fromYCbCr8 (Image w h pixels) =
-    Texture (fromIntegral w) (fromIntegral h) RGB pixels'
-  where
-    pixels' = fromList . map (toRGB . realToFrac
-
+    y' = realToFrac y
+    cr' = realToFrac cr - 128
+    cb' = realToFrac cb - 128
 throwError_ :: (MonadError Log m) => String -> m a
 throwError_ = throwError . Log ErrorLog CoreLog
 
