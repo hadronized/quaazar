6a66ea6fffc736093703737e5687520b399cfad3
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-03-26 23:26:55 +0100

    Don’t use YCC explicitely.

--- src/Quaazar/Core/Texture.hs
+++ src/Quaazar/Core/Texture.hs
@@ -21,6 +21,7 @@ module Quaazar.Core.Texture (
   ) where
 
 import Codec.Picture
+import Codec.Picture.Types
 import Control.Monad.Error.Class ( MonadError(..) )
 import Control.Monad.Trans ( liftIO )
 import Control.Lens
@@ -68,17 +69,17 @@ imageToTexture :: (MonadLogger m,MonadError Log m) => DynamicImage -> m Texture
 imageToTexture dynimg = do
   info CoreLog $ "texture type is " ++ showImageFormat dynimg
   case dynimg of
-    ImageY8 img -> return $ convertImage y8Converter img
-    ImageY16 img -> return $ convertImage y16Converter img
-    ImageYF img -> return $ convertImage yfConverter img
-    ImageYA8 img -> return $ convertImage ya8Converter img
-    ImageYA16 img -> return $ convertImage ya16Converter img
-    ImageRGB8 img -> return $ convertImage rgb8Converter img
-    ImageRGB16 img -> return $ convertImage rgb16Converter img
-    ImageRGBF img -> return $ convertImage rgbfConverter img
-    ImageRGBA8 img -> return $ convertImage rgba8Converter img
-    ImageRGBA16 img -> return $ convertImage rgba16Converter img
-    ImageYCbCr8 img -> return $ convertImage ycc8Converter img
+    ImageY8 img -> return $ convertImage_ y8Converter img
+    ImageY16 img -> return $ convertImage_ y16Converter img
+    ImageYF img -> return $ convertImage_ yfConverter img
+    ImageYA8 img -> return $ convertImage_ ya8Converter img
+    ImageYA16 img -> return $ convertImage_ ya16Converter img
+    ImageRGB8 img -> return $ convertImage_ rgb8Converter img
+    ImageRGB16 img -> return $ convertImage_ rgb16Converter img
+    ImageRGBF img -> return $ convertImage_ rgbfConverter img
+    ImageRGBA8 img -> return $ convertImage_ rgba8Converter img
+    ImageRGBA16 img -> return $ convertImage_ rgba16Converter img
+    ImageYCbCr8 img -> return $ convertImage_ rgb8Converter (convertImage img)
     _ -> throwError_ "unimplemented image format"
  
 showImageFormat :: DynamicImage -> String
@@ -100,11 +101,11 @@ imax8, imax16 :: Float
 imax8 = 1 / 255
 imax16 = 1 / realToFrac (maxBound :: Pixel16)
 
-convertImage :: forall a. (Pixel a)
+convertImage_ :: forall a. (Pixel a)
              => (a -> [Float])
              -> Image a
              -> Texture
-convertImage converter image@(Image w h _) = Texture w' h' texft pixels'
+convertImage_ converter image@(Image w h _) = Texture w' h' texft pixels'
   where
     w' = fromIntegral w
     h' = fromIntegral h
@@ -112,7 +113,7 @@ convertImage converter image@(Image w h _) = Texture w' h' texft pixels'
       1 -> R
       2 -> RG
       3 -> RGB
-      _ -> error "convertImage: more-than-3-components pixel"
+      _ -> error "convertImage_: more-than-3-components pixel"
     pixels' = fromList . concat $ [converter $ pixelAt image x y | x <- [0..w-1], y <- [0..h-1]]
 
 y8Converter :: Pixel8 -> [Float]
@@ -144,18 +145,6 @@ rgba8Converter (PixelRGBA8 r g b a) = map ((*imax8) . realToFrac) [r,g,b,a]
 
 rgba16Converter :: PixelRGBA16 -> [Float]
 rgba16Converter (PixelRGBA16 r g b a) = map ((*imax16) . realToFrac) [r,g,b,a]
-
-ycc8Converter :: PixelYCbCr8 -> [Float]
-ycc8Converter (PixelYCbCr8 y cb cr) =
-    [
-      y' + 1.402 * cr'
-    , y' - 0.34414 * cb' - 0.71414 * cr'
-    , y' + 1.772 * cb'
-    ]
-  where
-    y' = realToFrac y
-    cr' = realToFrac cr - 128
-    cb' = realToFrac cb - 128
 
 throwError_ :: (MonadError Log m) => String -> m a
 throwError_ = throwError . Log ErrorLog CoreLog
