d51f99bb5de8bf07b7579fb1c89772f969a05135
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-03-30 12:34:59 +0200

    Moved displayInto and swapBuffers (#74).
    They are now in Render.Frame. Deleted Render.Forward.IO.

--- src/Quaazar/Render/Forward.hs
+++ src/Quaazar/Render/Forward.hs
@@ -10,8 +10,7 @@
 ----------------------------------------------------------------------------
 
 module Quaazar.Render.Forward (
-    module Quaazar.Render.Forward.IO
-  , module Quaazar.Render.Forward.Lit
+    module Quaazar.Render.Forward.Lit
   , module Quaazar.Render.Forward.Looked
   , module Quaazar.Render.Forward.Post
   , module Quaazar.Render.Forward.Rendered
@@ -20,7 +19,6 @@ module Quaazar.Render.Forward (
   , module Quaazar.Render.Forward.Viewport
   ) where
 
-import Quaazar.Render.Forward.IO
 import Quaazar.Render.Forward.Lit
 import Quaazar.Render.Forward.Looked
 import Quaazar.Render.Forward.Post
--- src/Quaazar/Render/Frame.hs
+++ src/Quaazar/Render/Frame.hs
@@ -14,13 +14,21 @@ module Quaazar.Render.Frame where
 import Control.Lens
 import Control.Monad.Trans ( MonadIO(..) )
 import Control.Monad.Error.Class ( MonadError )
+import Data.Bits ( (.|.) )
 import Graphics.Rendering.OpenGL.Raw
+import qualified Graphics.UI.GLFW as GLFW ( swapBuffers )
 import Numeric.Natural ( Natural )
+import Quaazar.Render.Forward.Accumulation
+import Quaazar.Render.Forward.Post ( Post(..) )
+import Quaazar.Render.Forward.Renderer
+import Quaazar.Render.Forward.Viewport ( Viewport(..), setViewport )
 import Quaazar.Render.GL.Framebuffer (Target(..), bindFramebuffer
                                      , unbindFramebuffer )
 import Quaazar.Render.GL.Offscreen
+import Quaazar.Render.GL.Shader ( useProgram )
 import Quaazar.Render.GL.Texture ( Filter(..), Format(..), InternalFormat(..)
                                  , bindTextureAt )
+import Quaazar.Render.GL.VertexArray ( bindVertexArray )
 import Quaazar.Render.Texture ( GPUTexture(GPUTexture) )
 import Quaazar.Utils.Log
 import Quaazar.Utils.Scoped
@@ -49,3 +57,17 @@ gpuFrame w h = do
       (bindFramebuffer (off^.offscreenFB) ReadWrite)
       (GPUTexture . bindTextureAt $ off^.offscreenRender)
       (GPUTexture . bindTextureAt $ off^.offscreenDepthmap)
+
+displayInto :: (MonadIO m) => ForwardRenderer -> GPUFrame -> Viewport -> Post -> m ()
+displayInto (ForwardRenderer lighting accumulation _) gpuframe screenViewport pst = liftIO $ do
+  setViewport screenViewport
+  (finalOff,_) <- unPost pst lighting accumulation
+  useProgram (accumulation^.accumProgram)
+  useFrame gpuframe
+  glClear $ gl_DEPTH_BUFFER_BIT .|. gl_COLOR_BUFFER_BIT
+  bindTextureAt (finalOff^.offscreenRender) 0
+  bindVertexArray (accumulation^.accumVA)
+  glDrawArrays gl_TRIANGLE_STRIP 0 4
+
+swapBuffers :: (MonadIO m) => ForwardRenderer -> m ()
+swapBuffers fr = liftIO $ GLFW.swapBuffers (fr^.frWindow)
