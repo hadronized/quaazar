120401dc291f54612a9302a9bf4d1edeac02f6bb
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-04-07 17:42:21 +0200

    Added ShadowLOD.

--- src/Quaazar/Core/Light.hs
+++ src/Quaazar/Core/Light.hs
@@ -14,6 +14,8 @@ module Quaazar.Core.Light (
     -- * Lights
     Ambient(..)
   , Omni(..)
+    -- * Shadows
+  , ShadowLOD(..)
     -- * Resources
   , AmbientManager
   , OmniManager
@@ -45,7 +47,7 @@ instance Resource () Ambient
 type AmbientManager = Manager () Ambient
 
 -- |'Omni col pow rad'.
-data Omni = Omni Color Float Float Bool deriving (Eq,Show)
+data Omni = Omni Color Float Float (Maybe ShadowLOD) deriving (Eq,Show)
 
 instance FromJSON Omni where
   parseJSON = withObject "omni light" $ \o -> do
@@ -58,7 +60,7 @@ instance FromJSON Omni where
             <$> o .: "color"
             <*> o .: "power"
             <*> o .: "radius"
-            <*> o .:? "cast_shadows" .!= False
+            <*> o .:? "shadow_lod" .!= Nothing
         | otherwise = fail "not an omni light"
 
 instance Load Omni where
@@ -68,3 +70,26 @@ instance Load Omni where
 instance Resource () Omni
 
 type OmniManager = Manager () Omni
+
+-- |'ShadowLOD' represents the level of detail of a shadow. There’s currently
+-- three levels of detail:
+--
+--   - 'LowShadow' is a /low-detail/ shadow. You should use that LOD when you
+--     don’t need details (distant shadows or very big shadows);
+--   - 'MediumShadow' is a /medium-detail/ shadow. Used for common shadowing;
+--   - 'HighShadow' is a /high-detail/ shadow. You can use that LOD for close
+--     and/or small shadows the user might see pretty well.
+data ShadowLOD
+  = LowShadow
+  | MediumShadow
+  | HighShadow
+    deriving (Eq,Ord,Read,Show)
+
+instance FromJSON ShadowLOD where
+  parseJSON = withText "shadow level of detail" parseText
+    where
+      parseText t
+        | t == "low"    = return LowShadow
+        | t == "medium" = return MediumShadow
+        | t == "high"   = return HighShadow
+        | otherwise     = fail "unknown shadow level of detail"
