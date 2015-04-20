e7cc3ad6b6ab0391a24c52bbbc51756fde2ca314
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2015-04-20 12:12:13 +0200

    Haddock.

--- src/Quaazar/Render/GL/GLObject.hs
+++ src/Quaazar/Render/GL/GLObject.hs
@@ -7,6 +7,16 @@
 -- Stability   : experimental
 -- Portability : portable
 --
+-- OpenGL has a notion of “objects”. OpenGL objects tend to follow the
+-- same life-cycle pattern:
+--
+--   - they’re often created with a 'glGen*' or 'glCreate*' functions that
+--     write the OpenGL objects identifiers to a provided pointer
+--   - they’re often destroyed with a 'glDelete*' function that takes the
+--     previous pointer and frees up the memory
+--
+-- That pattern is captured with the 'GLObject' typeclass. It uses the
+-- 'MonadScoped' idea from "Quaazar.Utils.Scoped".
 ----------------------------------------------------------------------------
 
 module Quaazar.Render.GL.GLObject (
@@ -25,11 +35,12 @@ import Foreign.Ptr
 import Graphics.Rendering.OpenGL.Raw
 import Quaazar.Utils.Scoped
 
+-- |OpenGL object allocation/deallocation pattern.
 class GLObject a where
-  -- |Generate a new OpenGL object and put in it in scope.
+  -- |Generate a new OpenGL object.
   genObject :: (MonadScoped IO m) => m a
   genObject = fmap head (genObjects 1)
-  -- |Generate a few OpenGL objects and put them in scope.
+  -- |Generate a few OpenGL objects..
   genObjects :: (MonadScoped IO m) => Int -> m [a]
   genObjects n = replicateM n genObject
 
@@ -37,12 +48,12 @@ class GLObject a where
 -- objects to allocate and a pointer to the array to store the objects within
 -- (and the same for deallocating).
 --
--- This function captures that pattern for generice OpenGL objects.
+-- This function captures that pattern for generic OpenGL objects.
 genericGenObjects :: (MonadScoped IO m)
                   => Int -- ^ number of objects to allocate
                   -> (GLsizei -> Ptr GLuint -> IO ()) -- ^ allocator
                   -> (GLsizei -> Ptr GLuint -> IO ()) -- ^ deallocator
-                  -> (GLuint -> a) -- ^ Haskell wrapper
+                  -> (GLuint -> a) -- ^ wrapper
                   -> m [a]
 genericGenObjects n alloc dealloc wrapper = do
     p <- liftBase $ do
