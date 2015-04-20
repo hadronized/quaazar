d66d4bf09204191095590971dc50af023a8be47e
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2015-04-20 11:02:49 +0200

    Haddock.

--- src/Quaazar/Geometry/Position.hs
+++ src/Quaazar/Geometry/Position.hs
@@ -20,6 +20,7 @@ import Data.Aeson
 import Data.Aeson.Types ( typeMismatch )
 import Linear ( V3(..) )
 
+-- |Position in space a.k.a. space coordinates.
 newtype Position = Position { unPosition :: V3 Float } deriving (Eq,Ord,Show)
 
 instance FromJSON Position where
@@ -29,5 +30,6 @@ instance FromJSON Position where
       [x,y,z] -> return (pos x y z)
       _       -> typeMismatch "position" v
 
+-- |Build a 'Position' from /x/, /y/ and /z/ components.
 pos :: Float -> Float -> Float -> Position
 pos x y z = Position (V3 x y z)
