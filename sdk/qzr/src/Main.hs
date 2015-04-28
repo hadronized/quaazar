d2aa89ff642c46a783c5cee634270877c303dc8c
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-04-28 08:15:03 +0200

    Support 'new shader'.

--- sdk/qzr/src/Main.hs
+++ sdk/qzr/src/Main.hs
@@ -12,6 +12,7 @@
 import Control.Monad ( unless )
 import Data.Maybe ( fromMaybe )
 import Qzr.Init
+import Qzr.New.Shader
 import System.Environment ( getArgs )
 import System.FilePath
 
@@ -19,20 +20,26 @@ import Prelude hiding ( init )
 
 data Command
   = Init (Maybe FilePath)
+  | NewShader String
     deriving (Eq,Show)
 
 instance Read Command where
   readsPrec _ s = case words s of
       ("init":xs) -> processInit xs
+      ("new":xs) -> processNew xs
       _ -> []
     where
       processInit xs = case xs of
         [path] -> [(Init (Just path),"")]
         _ -> [(Init Nothing,"")]
+      processNew xs = case xs of
+        ["shader",name] -> [(NewShader name,"")]
+        _ -> []
 
 dispatchCommand :: Command -> IO ()
 dispatchCommand cmd = case cmd of
   Init path -> init (fromMaybe "./" path)
+  NewShader name -> newShader name
 
 main :: IO ()
 main = do
