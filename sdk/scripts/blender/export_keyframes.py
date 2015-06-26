b038fff228ba940dad0fb5ca418e8c25ed0078b2
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-06-27 01:46:44 +0200

    Adding more code to extract keyframes.

--- sdk/scripts/blender/export_keyframes.py
+++ sdk/scripts/blender/export_keyframes.py
@@ -40,9 +40,43 @@ class QuaazarKeyframesExporter(bpy.types.Operator, ExportHelper):
     , )
 
   def execute(self, context):
-    print("-- Quaazar Keyframes JSON Export --")
-    o = bpy.context.active_object
-    print("-- ----------------------- --")
+    actions = bpy.data.actions
+    for action in actions:
+      co = (0,0,0) # origin is the default
+      orient = (0,0,0,-1) # -Z axis is the default
+      scale = (1,1,1) # identity scale
+      print(action.name)
+
+      # Check whether we only have LocRotScale group
+      if not action.groups['LocRotScale'] or len(action.groups) != 1:
+        self.report({'ERROR'}, action.name + " should only have LocRotScale")
+        return {}
+
+      # Check that we have 10 channels
+      #   · location x
+      #   · location y
+      #   · location z
+      #   · orientation x
+      #   · orientation y
+      #   · orientation z
+      #   · orientation w
+      #   · scale x
+      #   · scale y
+      #   · scale z
+      channels = action.groups['LocRotScale'].channels
+      if len(channels) != 10:
+        self.report({'ERROR'}, action.name + " doesn’t have enough channels")
+        return {}
+      locXCurve = channels[0]
+      locYCurve = channels[1]
+      locZCurve = channels[2]
+      rotWCurve = channels[3]
+      rotXCurve = channels[4]
+      rotYCurve = channels[5]
+      rotZCurve = channels[6]
+      scaX      = channels[7]
+      scaY      = channels[8]
+      scaZ      = channels[9]
     return {'FINISHED'}
 
 def register():
@@ -53,4 +87,3 @@ def unregister():
 
 if __name__ == "__main__":
   register()
-
