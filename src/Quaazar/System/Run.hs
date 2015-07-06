60b2e6e5097cf7351e009b187de068e03c953fbc
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-07-06 23:45:02 +0200

    Refactored Run.hs.

--- src/Quaazar/System/Run.hs
+++ src/Quaazar/System/Run.hs
@@ -24,6 +24,25 @@ import Numeric.Natural ( Natural )
 import Quaazar.System.Event
 import Quaazar.Utils.Log
 
+-- |'standalone w h full title app' spawns a new stand-alone window with its own
+-- reaction system (based on "future"’s 'Future').
+--
+-- 'app' represents the application as a function. It’s passed several 'Future'
+-- values:
+--
+-- @
+--      Window
+--   -> Future (Key,KeyState)
+--   -> Future (MouseButton,MouseButtonState)
+--   -> Future (Double,Double)                -- (mouseX,mouseY)
+--   -> Future ()                             -- produce when closing the window
+--   -> Future Bool                           -- focused
+--   -> IO ()                                 -- events poller
+--   -> IO ()
+-- @
+--
+-- The @eventsPoller@ is used to poll events and make them available within the
+-- 'Future' values.
 standalone :: Natural -- ^ Width of the window
            -> Natural -- ^ Height of the window
            -> Bool -- ^ Should the window be fullscreen?
@@ -39,54 +58,79 @@ standalone :: Natural -- ^ Width of the window
               -> IO ()
               )
            -> IO ()
-standalone w h full title app = do
+standalone w h full title app =
+  initiate w h full title >>= \case
+    Just win -> do
+      initGL
+      (key,mouse,cursor,windowClosed,focus) <- getFutures win
+      app win key mouse cursor windowClosed focus pollEvents
+    Nothing -> terminate
+
+initiate :: Natural -> Natural -> Bool -> String -> IO (Maybe Window)
+initiate w h full title = do
   initiated <- GLFW.init
   if
     | initiated -> do
         glfwVersion <- fmap showGLFWVersion getVersion
-        print (Log InfoLog CoreLog $ "GLFW " ++ glfwVersion ++ " initialized!")
+        print $ Log InfoLog CoreLog $ "GLFW " ++ glfwVersion ++ " initialized!"
         monitor <- if full then getPrimaryMonitor else return Nothing
-        windowHint (WindowHint'ContextVersionMajor 4)
-        windowHint (WindowHint'ContextVersionMinor 4)
+        windowHint $ WindowHint'ContextVersionMajor 4
+        windowHint $ WindowHint'ContextVersionMinor 4
         createWindow (fromIntegral w) (fromIntegral h) title monitor Nothing >>= \win -> case win of
-          Just window -> makeContextCurrent win >> withWindow window app
-          Nothing -> print (Log ErrorLog CoreLog "unable to create window")
-        print (Log InfoLog CoreLog "bye!")
-        terminate
-    | otherwise -> print $ Log ErrorLog CoreLog "unable to init"
+          Just _ -> makeContextCurrent win >> pure win
+          Nothing -> do
+            print (Log ErrorLog CoreLog "unable to create window")
+            pure Nothing
+    | otherwise -> do
+      print $ Log ErrorLog CoreLog "unable to init"
+      pure Nothing
 
-withWindow :: Window
-           -> (
-                 Window
-              -> Future (Key,KeyState)
-              -> Future (MouseButton,MouseButtonState)
-              -> Future (Double,Double)
-              -> Future ()
-              -> Future Bool
-              -> IO ()
-              -> IO ()
+getFutures :: Window
+           -> IO (
+                Future (Key,KeyState)
+              , Future (MouseButton,MouseButtonState)
+              , Future (Double,Double)
+              , Future ()
+              , Future Bool
               )
-           -> IO ()
-withWindow window app = do
-    -- futures
-    (key,pushKey) <- newFuture
-    (mouse,clickMouse) <- newFuture
-    (cursor,moveCursor) <- newFuture
-    (windowClosed,closeWindow) <- newFuture
-    (focus,changeFocus) <- newFuture
-    -- callbacks
-    setKeyCallback window . Just $ \_ k _ s _ ->
-      pushKey (fromGLFWKey k,fromGLFWKeyState s)
-    setMouseButtonCallback window . Just $ \_ b s _ ->
-      clickMouse (fromGLFWMouseButton b,fromGLFWMouseButtonState s)
-    setCursorPosCallback window . Just $ \_ x y -> moveCursor (x,y)
-    setWindowCloseCallback window . Just $ \_ -> closeWindow ()
-    setWindowFocusCallback window . Just $ \_ f ->
-      changeFocus (fromGLFWFocusState f)
-    -- pre-process
-    initGL
-    -- run the whole shit
-    app window key mouse cursor windowClosed focus pollEvents
+getFutures win = do
+  -- futures
+  (key,pushKey) <- newFuture
+  (mouse,clickMouse) <- newFuture
+  (cursor,moveCursor) <- newFuture
+  (windowClosed,closeWindow) <- newFuture
+  (focus,changeFocus) <- newFuture
+  -- callbacks
+  setKeyCallback win . Just $ \_ k _ s _ ->
+    pushKey (fromGLFWKey k,fromGLFWKeyState s)
+  setMouseButtonCallback win . Just $ \_ b s _ ->
+    clickMouse (fromGLFWMouseButton b,fromGLFWMouseButtonState s)
+  setCursorPosCallback win . Just $ \_ x y -> moveCursor (x,y)
+  setWindowCloseCallback win . Just $ \_ -> closeWindow ()
+  setWindowFocusCallback win . Just $ \_ f ->
+    changeFocus (fromGLFWFocusState f)
+  pure (key,mouse,cursor,windowClosed,focus)
+
+-- |Interpreter version of 'standalone'.
+--
+-- That function can be used in a **ghci** session.
+{-
+standaloneInterpreter :: Natural -- ^ Width of the window
+                      -> Natural -- ^ Height of the window
+                      -> Bool -- ^ Should the window be fullscreen?
+                      -> String -- ^ Title of the window
+                      -> IO (
+                            Window
+                          , Future (Key,KeyState)
+                          , Future (MouseButton,MouseButtonState)
+                          , Future (Double,Double)
+                          , Future ()
+                          , Future Bool
+                          , IO ()
+                          )
+-}
+
+
 
 initGL :: IO ()
 initGL = do
